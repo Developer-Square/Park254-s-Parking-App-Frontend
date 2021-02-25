769cb151ae01e985b21de318f33f63b4d647accf
Ryan Njoroge <keddelyronjoz@gmail.com>
2021-02-25 08:32:59 +0300

    Added animated switcher to enable the user to switch between pages

--- lib/pages/registration_page.dart
+++ lib/pages/registration_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:keyboard_attachable/keyboard_attachable.dart';
 import '../components/globals_registration_login.dart' as globals;
 
 class RegistrationPage extends StatefulWidget {
@@ -8,20 +7,23 @@ class RegistrationPage extends StatefulWidget {
 }
 
 class _RegistrationPageState extends State<RegistrationPage> {
-  //Widget function that displays the inputs
-  Widget _buildFormField(String text) {
-    return Column(children: <Widget>[
-      Padding(
-        padding: const EdgeInsets.only(left: 20.0, right: 20.0),
-        child: Container(
-            padding: const EdgeInsets.only(left: 20.0, right: 20.0),
-            child: TextFormField(
-              decoration: InputDecoration(
-                  hintText: text,
-                  hintStyle: TextStyle(color: globals.placeHolderColor)),
-            )),
-      ),
-    ]);
+  int _step = 1;
+
+  changeScreens() {
+    if (_step == 1) {
+      return RegistrationScreens(
+          title: 'Email',
+          info: 'Welcome to our Parking app, kindly provide your email below',
+          step: _step);
+    } else if (_step == 2) {
+      return RegistrationScreens(
+          title: 'Verification',
+          info: 'Enter the code to verify your account',
+          step: _step);
+    } else if (_step == 3) {
+      return RegistrationScreens(
+          title: 'Password', info: 'Enter your password', step: _step);
+    }
   }
 
   Widget _buildSteps(String text) {
@@ -43,7 +45,6 @@ class _RegistrationPageState extends State<RegistrationPage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        resizeToAvoidBottomInset: false,
         backgroundColor: Colors.white,
         appBar: AppBar(
           backgroundColor: Colors.transparent,
@@ -52,10 +53,16 @@ class _RegistrationPageState extends State<RegistrationPage> {
               icon: Icon(Icons.arrow_back_ios),
               color: globals.fontColor,
               onPressed: () {
-                Navigator.of(context).pop();
+                setState(() {
+                  _step -= 1;
+                });
               }),
           title: Text(
-            'Email',
+            _step == 1
+                ? 'Email'
+                : _step == 2
+                    ? 'Verification'
+                    : 'Password',
             style: TextStyle(
                 color: globals.fontColor,
                 fontSize: 18.0,
@@ -63,58 +70,120 @@ class _RegistrationPageState extends State<RegistrationPage> {
           ),
           centerTitle: true,
         ),
-        body: FooterLayout(
-          footer: Text('hey'),
-          child: SingleChildScrollView(
-              child: Column(children: <Widget>[
-            SizedBox(height: 40.0),
-            Row(
-              children: <Widget>[
-                _buildSteps('STEP 1'),
-                _buildSteps('of 2'),
-              ],
-            ),
-            SizedBox(height: 170.0),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(left: 30.0, right: 30.0),
+        body: SingleChildScrollView(
+            child: Column(children: <Widget>[
+          SizedBox(height: 40.0),
+          Row(
+            children: <Widget>[
+              _buildSteps('STEP $_step'),
+              _buildSteps('of 3'),
+            ],
+          ),
+          SizedBox(height: 170.0),
+          AnimatedSwitcher(
+            child: changeScreens(),
+            key: ValueKey(_step),
+            duration: Duration(seconds: 1),
+            transitionBuilder: (widget, animation) =>
+                ScaleTransition(scale: animation, child: widget),
+          ),
+          InkWell(
+              onTap: () {
+                setState(() {
+                  _step += 1;
+                });
+              },
+              child: Container(
+                width: MediaQuery.of(context).size.width - 50,
+                height: 50.0,
+                decoration: BoxDecoration(
+                    color: globals.backgroundColor,
+                    borderRadius: BorderRadius.all(Radius.circular(25.0))),
+                child: Center(
                   child: Text(
-                    'Welcome to our Parking app, kindly provide your email below',
+                    _step == 3 ? 'Finish' : 'Next',
                     style: TextStyle(
                         color: globals.fontColor,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18.0),
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold),
                   ),
                 ),
-                SizedBox(height: 35.0),
-                _buildFormField('Email'),
-                SizedBox(height: 40.0),
-                InkWell(
-                    onTap: () {},
-                    child: Container(
-                      width: MediaQuery.of(context).size.width - 50,
-                      height: 50.0,
-                      decoration: BoxDecoration(
-                          color: globals.backgroundColor,
-                          borderRadius:
-                              BorderRadius.all(Radius.circular(25.0))),
-                      child: Center(
-                        child: Text(
-                          'Next',
-                          style: TextStyle(
-                              color: globals.fontColor,
-                              fontSize: 18.0,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                    ))
-              ],
-            )
-          ])),
-        ),
+              ))
+        ])),
       ),
+    );
+  }
+}
+
+class RegistrationScreens extends StatelessWidget {
+  final String title;
+  final String info;
+  final int step;
+
+  RegistrationScreens({this.title, this.info, this.step});
+
+  @override
+  Widget build(BuildContext context) {
+    //Widget function that displays the inputs
+    Widget _buildFormField(String text) {
+      return Column(children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+          child: Container(
+              padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+              child: text == 'Password'
+                  ? Column(
+                      children: [
+                        TextFormField(
+                          decoration: InputDecoration(
+                              labelText: 'Create password',
+                              labelStyle: TextStyle(
+                                  color: globals.backgroundColor,
+                                  fontSize: 19.0),
+                              hintText: text,
+                              hintStyle:
+                                  TextStyle(color: globals.placeHolderColor)),
+                        ),
+                        SizedBox(height: 15.0),
+                        TextFormField(
+                          decoration: InputDecoration(
+                              labelText: 'Confirm password',
+                              labelStyle: TextStyle(
+                                  color: globals.backgroundColor,
+                                  fontSize: 19.0),
+                              hintText: text,
+                              hintStyle:
+                                  TextStyle(color: globals.placeHolderColor)),
+                        ),
+                      ],
+                    )
+                  : TextFormField(
+                      decoration: InputDecoration(
+                          hintText: text,
+                          hintStyle:
+                              TextStyle(color: globals.placeHolderColor)),
+                    )),
+        ),
+      ]);
+    }
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Padding(
+          padding: EdgeInsets.only(left: 30.0, right: 30.0),
+          child: Text(
+            info,
+            style: TextStyle(
+                color: globals.fontColor,
+                fontWeight: FontWeight.bold,
+                fontSize: 18.0),
+          ),
+        ),
+        SizedBox(height: 35.0),
+        _buildFormField(title),
+        SizedBox(height: 40.0),
+      ],
     );
   }
 }
