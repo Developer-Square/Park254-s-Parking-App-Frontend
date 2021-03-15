43d7184631d00211a132dfd1ce7de34d697f3a65
linus saisi <42277372+saisilinus@users.noreply.github.com>
2021-03-15 17:30:30 +0300

    feat: add DottedHorizontalLine constructor

---
+++ lib/components/DottedHorizontalLine.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:park254_s_parking_app/config/globals.dart' as globals;
+
+class DotedHorizontalLine extends CustomPainter {
+  Paint _paint;
+  DotedHorizontalLine() {
+    _paint = Paint()
+      ..color = globals.textColor
+      ..strokeWidth = 2
+      ..strokeCap = StrokeCap.round;
+  }
+
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    for(double i = -300; i < 300; i = i + 15){
+      if(i% 3 == 0)
+        canvas.drawLine(Offset(i, 0.0), Offset(i+10, 0.0), _paint);
+    }
+  }
+
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return false;
+  }
+}
