93142188906914b68525b3343dd82c981caede18
saisilinus <saisilinus19@gmail.com>
2021-04-21 04:37:10 -0400

    feat: add forgot-password helper

---
+++ lib/functions/resetPassword.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'package:park254_s_parking_app/functions/handleError.dart';
+import '../config/globals.dart' as globals;
+
+Future<void> forgotPassword(String password, String token) async {
+  final url = Uri.parse(
+    '${globals.apiUrl}/v1/auth/reset-password?token=$token',
+  );
+  final response = await http.post(
+    url,
+    body: {'password': password},
+  );
+
+  if (response.statusCode == 204) {
+    throw 'success';
+  } else {
+    handleError(response.body);
+  }
+}
