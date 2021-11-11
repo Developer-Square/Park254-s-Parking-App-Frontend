48d8cf00bb5ea4c84500d9251a177a4794327adf
Ryan Njoroge <keddelyronjoz@gmail.com>
2021-11-11 09:52:34 +0300

    (feat): Moving from details from tecHive's account to Park254
    - Added Park254's details(Google services file for both android and ios)
    - Added new api keys for google maps for both Android and ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyCRv0qsKcr8DwaWi8rEA8vVnIYO1hkokx0")
+    GMSServices.provideAPIKey("AIzaSyBoQs2IZNwBOnO26D1LQBlcueBqvYmc6aM")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
